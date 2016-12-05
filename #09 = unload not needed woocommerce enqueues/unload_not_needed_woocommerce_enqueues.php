70ff100e6360006c0e5c870c5924e726706581db
Bogdan Dragomir <bogdanvdragomir@gmail.com>
2016-12-05 16:39:38 +0200

    [New Snippet] - Unload not needed woocommerce enqueues :sunglasses:

---
+++ #09 = unload not needed woocommerce enqueues/unload_not_needed_woocommerce_enqueues.php
@@ -0,0 +1,34 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'child_manage_woocommerce_styles', 99 );
+
+function child_manage_woocommerce_styles() {
+	//remove generator meta tag
+	remove_action( 'wp_head', array( $GLOBALS['woocommerce'], 'generator' ) );
+
+	//first check that woo exists to prevent fatal errors
+	if ( function_exists( 'is_woocommerce' ) ) {
+		//dequeue scripts and styles
+		if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() ) {
+			wp_dequeue_style( 'woocommerce_frontend_styles' );
+			wp_dequeue_style( 'woocommerce_fancybox_styles' );
+			wp_dequeue_style( 'woocommerce_chosen_styles' );
+			wp_dequeue_style( 'woocommerce_prettyPhoto_css' );
+			wp_dequeue_script( 'wc_price_slider' );
+			wp_dequeue_script( 'wc-single-product' );
+			wp_dequeue_script( 'wc-add-to-cart' );
+			wp_dequeue_script( 'wc-cart-fragments' );
+			wp_dequeue_script( 'wc-checkout' );
+			wp_dequeue_script( 'wc-add-to-cart-variation' );
+			wp_dequeue_script( 'wc-single-product' );
+			wp_dequeue_script( 'wc-cart' );
+			wp_dequeue_script( 'wc-chosen' );
+			wp_dequeue_script( 'woocommerce' );
+			wp_dequeue_script( 'prettyPhoto' );
+			wp_dequeue_script( 'prettyPhoto-init' );
+			wp_dequeue_script( 'jquery-blockui' );
+			wp_dequeue_script( 'jquery-placeholder' );
+			wp_dequeue_script( 'fancybox' );
+			wp_dequeue_script( 'jqueryui' );
+		}
+	}
+}
